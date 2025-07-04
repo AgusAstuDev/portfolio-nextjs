3096b4eb7a986ea9708f352a5820eeaae7e366ce
AgusAstuDev <agusastudillo.dev@gmail.com>
2025-07-03 21:11:05 -0300

    Se ha terminado con las animaciones correspondientes en el Hero

---
+++ src/components/common/ButtonLink.tsx
@@ -0,0 +1,27 @@
+import { forwardRef } from "react";
+import Image from "next/image";
+
+type ButtonLinkProps = {
+  href?: string;
+  text: string;
+  iconSrc?: string;
+  alt?: string;
+  onClick?: () => void;
+};
+
+// forwardRef permite que puedas pasar refs desde el componente padre
+export const ButtonLink = forwardRef<HTMLAnchorElement, ButtonLinkProps>(
+  ({ href = "#", text, iconSrc, alt = "", onClick }, ref) => {
+    return (
+      <a
+        ref={ref}
+        href={href}
+        onClick={onClick}
+        className="w-max h-auto flex gap-[10px] justify-center items-center px-[18px] py-[10px] text-secondary-color border-[var(--objects-border)] border-[1px] border-solid rounded-full bg-[var(--objects-background)] hover:bg-[var(--objects-background-hover)] cursor-pointer text-[13px] md:text-[15px] transform  transition-all duration-300 ease-in-out"
+      >
+        {iconSrc && <Image src={iconSrc} alt={alt} width={16} height={16} />}
+        {text}
+      </a>
+    );
+  }
+);
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,38 +5,34 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import SplitText from "gsap/src/SplitText";
 
+import { ButtonLink } from "../common/ButtonLink";
+
 gsap.registerPlugin(useGSAP, SplitText);
 
 export default function Hero() {
-  const titleRef = useRef(null);
-  const subtitleRef = useRef(null);
-  const descRef = useRef(null);
-  const contactBtnRef = useRef<HTMLAnchorElement>(null);
-  const cvBtnRef = useRef<HTMLAnchorElement>(null);
-  const arrowRef = useRef(null);
+  const helloRef = useRef(null!);
+  const titleRef = useRef(null!);
+  const subtitleRef = useRef(null!);
+  const descRef = useRef(null!);
+  const contactWrapperRef = useRef<HTMLDivElement>(null!);
+  const cvWrapperRef = useRef<HTMLDivElement>(null!);
+  const arrowRef = useRef(null!);
 
   useGSAP(() => {
-    if (
-      !titleRef.current ||
-      !subtitleRef.current ||
-      !descRef.current ||
-      !contactBtnRef.current ||
-      !cvBtnRef.current ||
-      !arrowRef.current
-    )
-      return;
-
+    const splitHello = new SplitText(helloRef.current, { type: "lines" });
     const splitTitle = new SplitText(titleRef.current, { type: "lines" });
     const splitSubtitle = new SplitText(subtitleRef.current, { type: "lines" });
     const splitDesc = new SplitText(descRef.current, { type: "lines" });
 
     const allLines = [
+      ...splitHello.lines,
       ...splitTitle.lines,
       ...splitSubtitle.lines,
       ...splitDesc.lines,
     ];
 
     gsap.from(allLines, {
+      delay: 0.5,
       y: 50,
       autoAlpha: 0,
       stagger: 0.2,
@@ -44,11 +40,11 @@ export default function Hero() {
       duration: 1,
     });
 
-    gsap.from([contactBtnRef.current, cvBtnRef.current], {
+    gsap.from([contactWrapperRef.current, cvWrapperRef.current], {
       y: 20,
       autoAlpha: 0,
       stagger: 0.15,
-      delay: 1.2,
+      delay: 1.7,
       duration: 0.8,
       ease: "power2.out",
     });
@@ -56,7 +52,7 @@ export default function Hero() {
     gsap.from(arrowRef.current, {
       y: 15,
       autoAlpha: 0,
-      duration: 1,
+      duration: 1.3,
       ease: "power1.inOut",
       repeat: -1,
       yoyo: true,
@@ -72,7 +68,10 @@ export default function Hero() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center px-[20px] gap-[10px] md:gap-[20px]">
       <div className="w-full h-auto flex flex-col justify-center items-center gap-[20px]">
-        <h2 className="text-sm md:text-base lg:text-lg text-center font-medium text-secondary-color">
+        <h2
+          ref={helloRef}
+          className="text-sm md:text-base lg:text-lg text-center font-medium text-secondary-color"
+        >
           Hola, soy
         </h2>
         <h1
@@ -96,30 +95,12 @@ export default function Hero() {
         </p>
       </div>
       <div className="w-max h-auto flex justify-center items-center gap-[15px] md:gap-[30px] mt-[30px]">
-        <a
-          ref={contactBtnRef}
-          className="w-max h-auto flex gap-[10px] justify-center items-center px-[18px] py-[10px] text-secondary-color border-[var(--objects-border)] border-[1px] border-solid rounded-full bg-[var(--objects-background)] text-[13px] md:text-[15px]"
-        >
-          <Image
-            src="/mail_icon.svg"
-            alt="Logo contact"
-            width={16}
-            height={16}
-          />
-          Contáctame
-        </a>
-        <a
-          ref={cvBtnRef}
-          className="w-max h-auto flex gap-[10px] justify-center items-center px-[18px] py-[10px] text-secondary-color border-[var(--objects-border)] border-[1px] border-solid rounded-full bg-[var(--objects-background)] text-[13px] md:text-[15px]"
-        >
-          <Image
-            src="/download_icon.svg"
-            alt="Logo download CV"
-            width={16}
-            height={16}
-          />
-          Ver mi CV
-        </a>
+        <div ref={contactWrapperRef}>
+          <ButtonLink text="Contáctame" iconSrc="/mail_icon.svg" />
+        </div>
+        <div ref={cvWrapperRef}>
+          <ButtonLink text="Ver mi CV" iconSrc="/download_icon.svg" />
+        </div>
       </div>
       <div
         ref={arrowRef}
